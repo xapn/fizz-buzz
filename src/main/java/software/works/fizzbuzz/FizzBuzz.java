16bfbd151eeabb9439680b3c308c3059cb69de56
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-12 13:29:48 +0200

    [Printing Fizz or Buzz] Refactor.

--- src/main/java/software/works/fizzbuzz/FizzBuzz.java
+++ src/main/java/software/works/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,14 @@ package software.works.fizzbuzz;
 
 public class FizzBuzz {
 
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+
     public String of(int number) {
         if (number % 3 == 0) {
-            return "Fizz";
+            return FIZZ;
         } else if (number % 5 == 0) {
-            return "Buzz";
+            return BUZZ;
         } else {
             return String.valueOf(number);
         }
--- src/test/java/software/works/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/software/works/fizzbuzz/FizzBuzzTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 
 public class FizzBuzzTest {
 
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+
     private FizzBuzz fizzBuzz;
 
     @Before
@@ -21,21 +24,21 @@ public class FizzBuzzTest {
 
     @Test
     public void should_get_fizz_given_3_as_number() {
-        assertThat(fizzBuzz.of(3)).isEqualTo("Fizz");
+        assertThat(fizzBuzz.of(3)).isEqualTo(FIZZ);
     }
 
     @Test
     public void should_get_fizz_given_number_multiple_of_3() {
-        assertThat(fizzBuzz.of(6)).isEqualTo("Fizz");
+        assertThat(fizzBuzz.of(6)).isEqualTo(FIZZ);
     }
 
     @Test
     public void should_get_buzz_given_5_as_number() {
-        assertThat(fizzBuzz.of(5)).isEqualTo("Buzz");
+        assertThat(fizzBuzz.of(5)).isEqualTo(BUZZ);
     }
 
     @Test
     public void should_get_buzz_given_number_multiple_of_5() {
-        assertThat(fizzBuzz.of(10)).isEqualTo("Buzz");
+        assertThat(fizzBuzz.of(10)).isEqualTo(BUZZ);
     }
 }
