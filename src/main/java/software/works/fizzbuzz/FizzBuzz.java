7b5d712679308101a45f84ab23ec72fd0a85e4f9
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-12 15:20:16 +0200

    [Printing Fizz or Buzz] Refactor.
    - Reduce duplications.

--- src/main/java/software/works/fizzbuzz/FizzBuzz.java
+++ src/main/java/software/works/fizzbuzz/FizzBuzz.java
@@ -5,19 +5,21 @@ public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
 
-    public String of(int number) {
-        if (number % 3 == 0 || number % 5 == 0) {
+    public String of(int value) {
+        Number number = new Number(value);
+
+        if (number.isMultipleOf3() || number.isMultipleOf5()) {
             return fizzIfMultipleOf3(number) + buzzIfMultipleOf5(number);
         } else {
             return String.valueOf(number);
         }
     }
 
-    private String fizzIfMultipleOf3(int number) {
-        return number % 3 == 0 ? FIZZ : "";
+    private String fizzIfMultipleOf3(Number number) {
+        return number.isMultipleOf3() ? FIZZ : "";
     }
 
-    private String buzzIfMultipleOf5(int number) {
-        return number % 5 == 0 ? BUZZ : "";
+    private String buzzIfMultipleOf5(Number number) {
+        return number.isMultipleOf5() ? BUZZ : "";
     }
 }
---
+++ src/main/java/software/works/fizzbuzz/Number.java
@@ -0,0 +1,31 @@
+package software.works.fizzbuzz;
+
+class Number {
+
+    private int value;
+    private boolean isMultipleOf3;
+    private boolean isMultipleOf5;
+
+    Number(int value) {
+        this.value = value;
+        checkIfThisNumberIsMultiple();
+    }
+
+    void checkIfThisNumberIsMultiple() {
+        isMultipleOf3 = value % 3 == 0;
+        isMultipleOf5 = value % 5 == 0;
+    }
+
+    public boolean isMultipleOf3() {
+        return isMultipleOf3;
+    }
+
+    public boolean isMultipleOf5() {
+        return isMultipleOf5;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
