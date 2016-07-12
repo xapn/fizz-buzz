bc748178bdbe74e777ed140727c32adcb70b30b9
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-12 15:47:23 +0200

    [Iterating] It should get 'Fizz' and 'Buzz' and numbers given a range of numbers.

--- src/main/java/software/works/fizzbuzz/FizzBuzz.java
+++ src/main/java/software/works/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
 
+    private int start;
+
     public String of(int value) {
         Number number = new Number(value);
 
@@ -21,5 +23,20 @@ public class FizzBuzz {
 
     private String buzzIfMultipleOf5(Number number) {
         return number.isMultipleOf5() ? BUZZ : "";
+    }
+
+    public FizzBuzz from(int start) {
+        this.start = start;
+        return this;
+    }
+
+    public String to(int end) {
+        StringBuilder result = new StringBuilder();
+
+        for (int value = start; value <= end; value++) {
+            result.append(of(value)).append(" ");
+        }
+
+        return result.substring(0, result.length() - 1).toString();
     }
 }
