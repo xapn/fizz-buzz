8f99589fe921467e50db7511f8465c5bf3b85d19
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-12 15:56:48 +0200

    [Iterating] Play at FizzBuzz from 1 to 100.

---
+++ src/main/java/software/works/fizzbuzz/Example.java
@@ -0,0 +1,10 @@
+package software.works.fizzbuzz;
+
+public class Example {
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        System.out.println(fizzBuzz.from(1).to(100));
+    }
+}
