2d26905a63a00affbfe7ddca913fbabb947f6b6c
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-12 15:03:28 +0200

    [Printing Fizz or Buzz] Refactor.
    - Reduce duplications.

--- src/main/java/software/works/fizzbuzz/FizzBuzz.java
+++ src/main/java/software/works/fizzbuzz/FizzBuzz.java
@@ -6,14 +6,18 @@ public class FizzBuzz {
     private static final String BUZZ = "Buzz";
 
     public String of(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return FIZZ + BUZZ;
-        } else if (number % 3 == 0) {
-            return FIZZ;
-        } else if (number % 5 == 0) {
-            return BUZZ;
+        if (number % 3 == 0 || number % 5 == 0) {
+            return fizzIfMultipleOf3(number) + buzzIfMultipleOf5(number);
         } else {
             return String.valueOf(number);
         }
+    }
+
+    private String fizzIfMultipleOf3(int number) {
+        return number % 3 == 0 ? FIZZ : "";
+    }
+
+    private String buzzIfMultipleOf5(int number) {
+        return number % 5 == 0 ? BUZZ : "";
     }
 }
