78ab6aba8b568ab082680e6a891e045ff28702c2
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-13 21:06:17 +0200

    [Combining variations] It should print 'FizzFizzBuzz' for 30.

--- src/test/java/software/works/fizzbuzz/FizzBuzzWhenCombiningVariationsTest.java
+++ src/test/java/software/works/fizzbuzz/FizzBuzzWhenCombiningVariationsTest.java
@@ -18,4 +18,19 @@ public class FizzBuzzWhenCombiningVariationsTest {
     public void should_get_fizzfizz_given_3_as_number() {
         assertThat(fizzBuzz.of(3)).isEqualTo("FizzFizz");
     }
+
+    @Test
+    public void should_get_fizzbuzzbuzz_given_15_as_number() {
+        assertThat(fizzBuzz.of(15)).isEqualTo("FizzBuzzBuzz");
+    }
+
+    @Test
+    public void should_get_fizzfizzbuzz_given_30_as_number() {
+        assertThat(fizzBuzz.of(30)).isEqualTo("FizzFizzBuzz");
+    }
+
+    @Test
+    public void should_get_fizzfizzbuzzbuzz_given_315_as_number() {
+        assertThat(fizzBuzz.of(315)).isEqualTo("FizzFizzBuzzBuzz");
+    }
 }
