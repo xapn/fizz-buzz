71b7f29808ab47fbba31b6b3b40af1a08493e41b
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-15 20:59:55 +0200

    [Combining variations] Refactor.
    - Simplify the recording of all player predicates.

--- src/main/java/software/works/fizzbuzz/rule/VariationsCombiningPlayer.java
+++ src/main/java/software/works/fizzbuzz/rule/VariationsCombiningPlayer.java
@@ -21,20 +21,30 @@ public class VariationsCombiningPlayer implements Player {
     private List<FizzBuzzPredicate> recordPredicates(List<Player> players) {
         List<FizzBuzzPredicate> allPredicates = new ArrayList<>();
 
-        Optional<Integer> numberOfPredicatesByPlayer = players.stream() //
-                .map(player -> ((AbstractPlayer) player).getPredicates().size()) //
-                .max(Comparator.naturalOrder());
-
-        for (int index = 0; index < numberOfPredicatesByPlayer.get(); index++) {
-            for (Player player : players) {
-                List<FizzBuzzPredicate> playerPredicates = ((AbstractPlayer) player).getPredicates();
-                if (index < playerPredicates.size()) {
-                    allPredicates.add(playerPredicates.get(index));
-                }
-            }
+        for (int index = 0; index < maximalNumberOfPredicatesByPlayer(players).get(); index++) {
+            allPredicates.addAll(onePredicateByPlayer(index, players));
         }
 
         return allPredicates;
+    }
+
+    private Optional<Integer> maximalNumberOfPredicatesByPlayer(List<Player> players) {
+        return players.stream() //
+                .map(player -> ((AbstractPlayer) player).getPredicates().size()) //
+                .max(Comparator.naturalOrder());
+    }
+
+    private List<FizzBuzzPredicate> onePredicateByPlayer(int index, List<Player> players) {
+        List<FizzBuzzPredicate> predicatesOneByPlayer = new ArrayList<>(players.size());
+
+        for (Player player : players) {
+            List<FizzBuzzPredicate> playerPredicates = ((AbstractPlayer) player).getPredicates();
+            if (index < playerPredicates.size()) {
+                predicatesOneByPlayer.add(playerPredicates.get(index));
+            }
+        }
+
+        return predicatesOneByPlayer;
     }
 
     FizzBuzzPredicate wordIf(String word, Predicate<Integer> predicate) {
