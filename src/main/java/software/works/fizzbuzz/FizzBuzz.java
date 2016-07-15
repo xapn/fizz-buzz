86423ca358db810aec048220fa4fecdf70507246
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-15 18:11:22 +0200

    [Combining variations] Refactor.
    - Simplify the choice of the player by delegating it to a builder.

--- src/main/java/software/works/fizzbuzz/FizzBuzz.java
+++ src/main/java/software/works/fizzbuzz/FizzBuzz.java
@@ -1,61 +1,32 @@
 package software.works.fizzbuzz;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import software.works.fizzbuzz.rule.DigitPlayer;
 import software.works.fizzbuzz.rule.DivisionPlayer;
-import software.works.fizzbuzz.rule.VariationsCombiningPlayer;
+import software.works.fizzbuzz.rule.PlayerBuilder;
 
 public class FizzBuzz {
 
-    private List<Player> players;
+    private PlayerBuilder playerBuilder;
 
     public FizzBuzz() {
-        players = new ArrayList<>();
+        playerBuilder = new PlayerBuilder();
     }
 
     public String of(int value) {
-        return chosenPlayer().playAtFizzBuzz(value);
+        return playerBuilder.chosenPlayer().playAtFizzBuzz(value);
     }
 
     public FizzBuzzRange from(int start) {
-        return new FizzBuzzRange(chosenPlayer()).from(start);
+        return new FizzBuzzRange(playerBuilder.chosenPlayer()).from(start);
     }
 
     public FizzBuzz whenNumberHasFactors() {
-        players.add(new DivisionPlayer());
+        playerBuilder.append(new DivisionPlayer());
         return this;
     }
 
     public FizzBuzz whenNumberContainsDigits() {
-        players.add(new DigitPlayer());
+        playerBuilder.append(new DigitPlayer());
         return this;
-    }
-
-    private Player chosenPlayer() {
-        Player player = combineVariations(players);
-        return chooseClassicPlayerByDefaultIfUnknown(player);
-    }
-
-    private Player combineVariations(List<Player> players) {
-        Player player = null;
-
-        if (!players.isEmpty()) {
-            if (players.size() == 1) {
-                player = players.get(0);
-            } else {
-                player = new VariationsCombiningPlayer(players);
-            }
-        }
-
-        return player;
-    }
-
-    private Player chooseClassicPlayerByDefaultIfUnknown(Player player) {
-        if (player == null) {
-            player = new DivisionPlayer();
-        }
-        return player;
     }
 }
