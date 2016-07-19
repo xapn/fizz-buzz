db8faff4a999e6d2ad62b256614ce43596103c08
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-19 15:18:45 +0200

    [Printing words only once] It should print 'Fizz', 'Buzz' or 'FizzBuzz' given some arbitrary numbers or a range of numbers.

--- src/test/java/software/works/fizzbuzz/FizzBuzzWordsPrintedOnlyOnceTest.java
+++ src/test/java/software/works/fizzbuzz/FizzBuzzWordsPrintedOnlyOnceTest.java
@@ -21,4 +21,14 @@ public class FizzBuzzWordsPrintedOnlyOnceTest {
     public void should_get_fizz_given_3_as_number_containing_and_divisible_by_3() {
         assertThat(fizzBuzz.of(3)).isEqualTo("Fizz");
     }
+
+    @Test
+    public void should_get_fizzbuzz_given_some_arbitrary_numbers() {
+        assertThat(fizzBuzz.of(45, 315)).isEqualTo("FizzBuzz FizzBuzz");
+    }
+
+    @Test
+    public void should_get_fizz_or_buzz_given_a_range_of_numbers() {
+        assertThat(fizzBuzz.from(1).to(15)).isEqualTo("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz Fizz 14 FizzBuzz");
+    }
 }
