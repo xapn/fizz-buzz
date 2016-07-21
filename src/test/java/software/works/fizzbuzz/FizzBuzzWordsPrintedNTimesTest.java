c6cebff3d2e0ebfe6ce5e5e8cb51e3aec935d78f
Xavier Pigeon <xpigeon@wemanity.com>
2016-07-21 19:16:07 +0200

    [Printing words N times] It should print 'Fizz' or 'Buzz' given arbitrary numbers or a range of numbers.

--- src/test/java/software/works/fizzbuzz/FizzBuzzWordsPrintedNTimesTest.java
+++ src/test/java/software/works/fizzbuzz/FizzBuzzWordsPrintedNTimesTest.java
@@ -14,6 +14,10 @@ public class FizzBuzzWordsPrintedNTimesTest {
         fizzBuzz = new FizzBuzz().wordsNTimes();
     }
 
+    public void should_get_number_given_1_as_number() {
+        assertThat(fizzBuzz.of(1)).isEqualTo("1");
+    }
+
     @Test
     public void should_get_fizzfizz_given_9_as_number() {
         assertThat(fizzBuzz.whenNumberHasFactors().of(9)).isEqualTo("FizzFizz");
@@ -22,5 +26,28 @@ public class FizzBuzzWordsPrintedNTimesTest {
     @Test
     public void should_get_fizzfizz_given_331_as_number() {
         assertThat(fizzBuzz.whenNumberContainsDigits().of(331)).isEqualTo("FizzFizz");
+    }
+
+    @Test
+    public void should_get_fizzfizzfizz_given_33_as_number() {
+        assertThat(fizzBuzz.whenNumberHasFactors().whenNumberContainsDigits().of(33)).isEqualTo("FizzFizzFizz");
+    }
+
+    @Test
+    public void should_get_fizzfizzbuzzbuzzbuzz_given_3355_as_number() {
+        assertThat(fizzBuzz.whenNumberHasFactors().whenNumberContainsDigits().of(3355))
+                .isEqualTo("FizzFizzBuzzBuzzBuzz");
+    }
+
+    @Test
+    public void should_fizz_or_buzz_given_some_arbitrary_numbers() {
+        assertThat(fizzBuzz.whenNumberHasFactors().whenNumberContainsDigits().of(1, 3, 5))
+                .isEqualTo("1 FizzFizz BuzzBuzz");
+    }
+
+    @Test
+    public void should_fizz_or_buzz_given_a_range_of_numbers() {
+        assertThat(fizzBuzz.whenNumberHasFactors().whenNumberContainsDigits().from(1).to(15))
+                .isEqualTo("1 2 FizzFizz 4 BuzzBuzz Fizz 7 8 FizzFizz Buzz 11 Fizz Fizz 14 FizzBuzzBuzz");
     }
 }
