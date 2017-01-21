93afd1ba9eaff84615052f83b4b3e7a4133fefd5
Xavier Pigeon <xpigeon@wemanity.com>
2017-01-21 22:47:09 +0100

    [Specifying alternate words] It should get Fizz or Buzz or Woof and Foo or Bar given 2 sets of alternate words.

--- src/test/java/software/works/fizzbuzz/FizzBuzzPlayingWithAlternateWordsTest.java
+++ src/test/java/software/works/fizzbuzz/FizzBuzzPlayingWithAlternateWordsTest.java
@@ -21,4 +21,11 @@ public class FizzBuzzPlayingWithAlternateWordsTest {
         assertThat(fizzBuzz.alternateWords(Arrays.asList("Foo", "Bar"), 2).from(1).to(6))
                 .isEqualTo("1 Foo 3 Bar 5 Foo");
     }
+
+    @Test
+    public void should_get_fizz_or_buzz_or_woof_and_foo_or_bar_given_2_sets_of_alternate_words() {
+        assertThat(fizzBuzz.alternateWords(Arrays.asList("Fizz", "Buzz", "Woof"), 3)
+                .alternateWords(Arrays.asList("Foo", "Bar"), 5).from(1).to(15))
+                        .isEqualTo("1 2 Fizz 4 Foo Buzz 7 8 Woof Bar 11 Fizz 13 14 BuzzFoo");
+    }
 }
