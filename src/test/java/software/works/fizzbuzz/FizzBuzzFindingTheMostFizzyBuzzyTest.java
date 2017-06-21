164feed7e4dc4671c943395d8d96c3c8fbd0bd73
Xavier Pigeon <xpn.projects@gmail.com>
2017-06-21 19:06:51 +0200

    Refactor some tests by using TestAsYouThink.

--- src/test/java/software/works/fizzbuzz/FizzBuzzFindingTheMostFizzyBuzzyTest.java
+++ src/test/java/software/works/fizzbuzz/FizzBuzzFindingTheMostFizzyBuzzyTest.java
@@ -2,67 +2,78 @@ package software.works.fizzbuzz;
 
 import org.junit.Before;
 import org.junit.Test;
+import testasyouthink.GivenWhenThenDsl.PreparationStage.Given;
+import testasyouthink.TestAsYouThink;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static testasyouthink.TestAsYouThink.givenSutClass;
 
 public class FizzBuzzFindingTheMostFizzyBuzzyTest {
 
-    private FizzBuzz fizzBuzz;
+    private Given<FizzBuzz> givenFizzBuzzAsSut;
 
     @Before
     public void prepareFixtures() {
-        fizzBuzz = new FizzBuzz();
+        givenFizzBuzzAsSut = givenSutClass(FizzBuzz.class);
     }
 
     @Test
     public void should_find_the_alone_fizzbuzzified_number_given_some_arbitrary_numbers() {
-        // WHEN
-        TheMostFizzyBuzzy result = fizzBuzz
-                .findTheMostFizzyBuzzy()
-                .in(1, 3, 4);
-
-        // THEN
-        assertThat(result.asNumber()).hasValue("3");
-        assertThat(result.asWord()).hasValue("Fizz");
+        givenFizzBuzzAsSut
+                .when(sut -> {
+                    return sut
+                            .findTheMostFizzyBuzzy()
+                            .in(1, 3, 4);
+                })
+                .then(result -> {
+                    assertThat(result.asNumber()).hasValue("3");
+                    assertThat(result.asWord()).hasValue("Fizz");
+                });
     }
 
     @Test
     public void should_find_no_fizzbuzzifiable_number_given_some_arbitrary_numbers() {
-        // WHEN
-        TheMostFizzyBuzzy result = fizzBuzz
-                .findTheMostFizzyBuzzy()
-                .in(1, 2, 4);
-
-        // THEN
-        assertThat(result.get()).isEmpty();
-        assertThat(result.asNumber()).isEmpty();
-        assertThat(result.asWord()).isEmpty();
+        givenFizzBuzzAsSut
+                .when(sut -> {
+                    return sut
+                            .findTheMostFizzyBuzzy()
+                            .in(1, 2, 4);
+                })
+                .then(result -> {
+                    assertThat(result.get()).isEmpty();
+                    assertThat(result.asNumber()).isEmpty();
+                    assertThat(result.asWord()).isEmpty();
+                });
     }
 
     @Test
     public void should_find_the_most_fizzybuzzy_given_at_least_some_fizzbuzzifiable_numbers() {
-        // WHEN
-        TheMostFizzyBuzzy result = fizzBuzz
-                .findTheMostFizzyBuzzy()
-                .in(1, 3, 4, 5, 15, 20, 22);
-
-        // THEN
-        assertThat(result.asNumber()).hasValue("15");
-        assertThat(result.asWord()).hasValue("FizzBuzz");
+        givenFizzBuzzAsSut
+                .when(sut -> {
+                    return sut
+                            .findTheMostFizzyBuzzy()
+                            .in(1, 3, 4, 5, 15, 20, 22);
+                })
+                .then(result -> {
+                    assertThat(result.asNumber()).hasValue("15");
+                    assertThat(result.asWord()).hasValue("FizzBuzz");
+                });
     }
 
     @Test
     public void should_find_the_most_fizzybuzzy_given_a_range_of_numbers() {
-        // WHEN
-        TheMostFizzyBuzzy result = fizzBuzz
-                .whenNumberHasFactors()
-                .whenNumberContainsDigits()
-                .findTheMostFizzyBuzzy()
-                .from(1)
-                .to(15);
-
-        // THEN
-        assertThat(result.asNumber()).hasValue("15");
-        assertThat(result.asWord()).hasValue("FizzBuzzBuzz");
+        givenFizzBuzzAsSut
+                .when(sut -> {
+                    return sut
+                            .whenNumberHasFactors()
+                            .whenNumberContainsDigits()
+                            .findTheMostFizzyBuzzy()
+                            .from(1)
+                            .to(15);
+                })
+                .then(result -> {
+                    assertThat(result.asNumber()).hasValue("15");
+                    assertThat(result.asWord()).hasValue("FizzBuzzBuzz");
+                });
     }
 }
